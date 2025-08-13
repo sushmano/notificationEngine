ba5b10a33099ebba373a57d0df54f17a3b0fc400
sushmano <sushmasweety2019@gmail.com>
2025-08-13 10:02:16 +0530

    Create KStreamsNotificationApp.java

---
+++ src/main/java/com/example/notifyks/KStreamsNotificationApp.java
@@ -0,0 +1,15 @@
+package com.example.notifyks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
+
+@SpringBootApplication
+@EnableKafka
+@EnableKafkaStreams
+public class KStreamsNotificationApp {
+    public static void main(String[] args) {
+        SpringApplication.run(KStreamsNotificationApp.class, args);
+    }
+}
