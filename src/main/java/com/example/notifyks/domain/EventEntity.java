78d359ff198ec6e1d1fc65d720348d98d19d428b
sushmano <sushmasweety2019@gmail.com>
2025-08-13 11:02:49 +0530

    Update EventEntity.java

--- src/main/java/com/example/notifyks/domain/EventEntity.java
+++ src/main/java/com/example/notifyks/domain/EventEntity.java
@@ -16,16 +16,3 @@ public class EventEntity {
     @Enumerated(EnumType.STRING)
     public com.example.notifyks.domain.Enums.EventPriority priority = com.example.notifyks.domain.Enums.EventPriority.MEDIUM;
 }
-
-@Entity @Table(name="notifications")
-public class NotificationEntity {
-    @Id public String id = UUID.randomUUID().toString();
-    public String eventId;
-    @Enumerated(EnumType.STRING)
-    public com.example.notifyks.domain.Enums.ChannelType channel;
-    @Enumerated(EnumType.STRING)
-    public com.example.notifyks.domain.Enums.NotificationStatus status = com.example.notifyks.domain.Enums.NotificationStatus.PENDING;
-    public int attempts = 0;
-    public Instant lastUpdated = Instant.now();
-    @Lob public String lastError;
-}
