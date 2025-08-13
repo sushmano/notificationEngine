706f75e90a2ea7e065abc9ac5c47dbc1960701e3
sushmano <sushmasweety2019@gmail.com>
2025-08-13 10:16:30 +0530

    Create Repos.java

---
+++ src/main/java/com/example/notifyks/repo/Repos.java
@@ -0,0 +1,11 @@
+package com.example.notifyks.repo;
+
+import com.example.notifyks.domain.EventEntity;
+import com.example.notifyks.domain.NotificationEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface EventRepository extends JpaRepository<EventEntity, String> {}
+public interface NotificationRepository extends JpaRepository<NotificationEntity, String> {
+    List<NotificationEntity> findByEventId(String eventId);
+}
