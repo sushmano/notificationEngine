624bee3e35bbed13225e57990700ce2f285338a8
sushmano <sushmasweety2019@gmail.com>
2025-08-13 10:54:34 +0530

    Create EventRepository.java

---
+++ src/main/java/com/example/notifyks/repo/EventRepository.java
@@ -0,0 +1,9 @@
+package com.example.notifyks.repo;
+
+import com.example.notifyks.domain.EventEntity;
+import com.example.notifyks.domain.NotificationEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface EventRepository extends JpaRepository<EventEntity, String> {
+}
