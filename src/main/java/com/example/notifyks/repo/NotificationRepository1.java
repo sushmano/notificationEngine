01dbf18c286c6be324507784c15807884aebcd3c
sushmano <sushmasweety2019@gmail.com>
2025-08-13 10:56:14 +0530

    Update and rename Repos.java to NotificationRepository1.java

--- src/main/java/com/example/notifyks/repo/NotificationRepository1.java
+++ src/main/java/com/example/notifyks/repo/NotificationRepository1.java
@@ -5,7 +5,6 @@ import com.example.notifyks.domain.NotificationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-public interface EventRepository extends JpaRepository<EventEntity, String> {}
-public interface NotificationRepository extends JpaRepository<NotificationEntity, String> {
+public interface NotificationRepository1 extends JpaRepository<NotificationEntity, String> {
     List<NotificationEntity> findByEventId(String eventId);
 }
